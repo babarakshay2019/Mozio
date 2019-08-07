1f108440ccb2c20d0a778724b0400e3d01fdd75e
akshay <akshay@example.com>
2019-08-07 14:53:00 +0530

    All provide and service apis

---
+++ provider/views.py
@@ -0,0 +1,33 @@
+from rest_framework import viewsets, generics
+from .models import Provider, ServiceArea
+from .serializers import ProviderSerializer, ServiceAreaSerializer
+from django.contrib.gis.geos import Point
+
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    """
+    A user can perform CURD operation in Provider
+    """
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+
+class ServiceAreaViewSet(viewsets.ModelViewSet):
+    """
+    A user can perform CURD operation in ServiceArea
+    """
+    queryset = ServiceArea.objects.all()
+    serializer_class = ServiceAreaSerializer
+
+
+class ServiceAreaList(generics.ListAPIView):
+    """
+    A ListApiView to search area.
+    """
+    serializer_class = ServiceAreaSerializer
+
+    def get_queryset(self):
+        lat = self.request.GET.get('lat')
+        lon = self.request.GET.get('lon')
+        pnt_wkt = Point(float(lat), float(lon))
+        return ServiceArea.objects.filter(mpoly__intersects=pnt_wkt)
